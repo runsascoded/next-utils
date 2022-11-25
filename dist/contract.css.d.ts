bc7175001420879720c822a03335f38d465a3581
Ryan Williams <ryan@runsascoded.com>
2022-11-25 12:16:48 -0500

    add dist/contract.css*

---
+++ dist/contract.css.d.ts
@@ -0,0 +1,7 @@
+export declare const vars: {
+    color: `var(--${string})` | `var(--${string}, ${string})` | `var(--${string}, ${number})`;
+    linkColor: `var(--${string})` | `var(--${string}, ${string})` | `var(--${string}, ${number})`;
+    backgroundColor: `var(--${string})` | `var(--${string}, ${string})` | `var(--${string}, ${number})`;
+};
+export declare const dark: string;
+export declare const light: string;
