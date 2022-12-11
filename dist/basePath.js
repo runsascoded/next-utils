79b143a27343266113a540798890827aaf72f1f7
Ryan Williams <ryan@runsascoded.com>
2022-12-11 18:26:12 -0500

    basePath fallback

--- dist/basePath.js
+++ dist/basePath.js
@@ -1,6 +1,8 @@
 import getConfig from "next/config";
 export function getBasePath() {
     const { publicRuntimeConfig: config } = getConfig();
+    if (!config)
+        return "";
     const { basePath = "" } = config;
     return basePath;
 }
--- src/basePath.tsx
+++ src/basePath.tsx
@@ -2,6 +2,7 @@ import getConfig from "next/config";
 
 export function getBasePath() {
     const { publicRuntimeConfig: config } = getConfig()
+    if (!config) return ""
     const { basePath = "" } = config
     return basePath
 }
