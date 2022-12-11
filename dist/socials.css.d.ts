bc1c73983fb8b6017557f1cff3d81be999e7ec17
Ryan Williams <ryan@runsascoded.com>
2022-12-11 17:12:47 -0500

    add <Socials>

---
+++ dist/socials.css.d.ts
@@ -0,0 +1,4 @@
+export declare const socials: string;
+export declare const social = "social";
+export declare const logo = "logo";
+export declare const svg = "svg";
---
+++ src/socials.css.ts
@@ -0,0 +1,25 @@
+import {globalStyle, style} from '@vanilla-extract/css'
+
+export const socials = style({
+    listStyle: "none",
+    display: "inline-block",
+    padding: 0,
+})
+
+export const social = "social"
+globalStyle(`${socials} .${social}`, {
+    display: "inline-block",
+    margin: "0 0.3em",
+})
+
+export const logo = "logo"
+globalStyle(`${socials} .${logo}`, {
+    width: "3em",
+})
+
+export const svg = "svg"
+globalStyle(`${socials} .${svg}`, {
+    width: "3em",
+})
+
+//export default { socials, social, logo }
