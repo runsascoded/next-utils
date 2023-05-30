41790a3ac3aa98ace3ce73f901b92cb579364cba
Ryan Williams <ryan@runsascoded.com>
2023-05-30 09:47:54 -0400

    fix external links

--- dist/a.js
+++ dist/a.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 export default function A({ href, ref, children, ...attrs }) {
-    if (href && href.startsWith("/")) {
+    if (href && (href.startsWith("/") || href.startsWith("#"))) {
         if (ref) {
             console.warn(`Dropping anchor ref ${ref}`, { href, ...attrs });
         }
@@ -9,6 +9,6 @@ export default function A({ href, ref, children, ...attrs }) {
     }
     else {
         const { target = "_blank", rel = "noreferrer", ...rest } = attrs;
-        return React.createElement("a", { target: target, rel: rel, ref: ref, ...rest }, children);
+        return React.createElement("a", { href: href, target: target, rel: rel, ref: ref, ...rest }, children);
     }
 }
--- src/a.tsx
+++ src/a.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 export type AProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
 
 export default function A({ href, ref, children, ...attrs }: AProps) {
-    if (href && href.startsWith("/")) {
+    if (href && (href.startsWith("/") || href.startsWith("#"))) {
         if (ref) {
             console.warn(`Dropping anchor ref ${ref}`, { href, ...attrs })
         }
         return <Link href={href} {...attrs}>{children}</Link>
     } else {
         const { target = "_blank", rel = "noreferrer", ...rest } = attrs
-        return <a target={target} rel={rel} ref={ref} {...rest}>{children}</a>
+        return <a href={href} target={target} rel={rel} ref={ref} {...rest}>{children}</a>
     }
 }
