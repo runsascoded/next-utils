6ccefd619ebf1769bf232aac6a3cabd167484193
Ryan Williams <ryan@runsascoded.com>
2023-09-15 11:41:15 -0400

    write-redirects: optional trailingSlash configuration

--- write-redirects.js
+++ write-redirects.js
@@ -3,21 +3,21 @@
 // Load "redirects.json", write simple HTML files into the "out" directory, containing a meta redirect tag.
 // Can be used after a `next export` to create a static site with redirects.
 import fs from "fs"
-const { entries } = Object
-
+import { dirname } from "path"
 import program from 'commander';
 
-const cwd = process.cwd()
+const { entries } = Object
 
 const options =
     program
         .option('-b, --base-path <path>', 'Base path to prepend to all redirects')
         .option('-o, --out-dir <path>', 'Directory to write redirects to; default: out')
         .option('-f, --file <path>', 'File to read redirects from; default: redirects.json')
+        .option('-s, --trailing-slash', 'Write redirects to <out>/<dst>/index.html instead of <out>/<dst>.html; use when next.config.js has `trailingSlash: true`')
         .parse(process.argv)
         .opts()
 
-let { basePath = "/", outDir = "out", file = "redirects.json" } = options
+let { basePath = "/", outDir = "out", file = "redirects.json", trailingSlash, } = options
 const redirects = JSON.parse(fs.readFileSync(file).toString())
 console.log(redirects)
 
@@ -31,8 +31,17 @@ entries(redirects).map(([ src, dst ]) => {
     if (!dst.startsWith("/")) {
         dst = `/${dst}`
     }
-    const dir = `${outDir}${basePath}${src}`
-    const outPath = `${dir}/index.html`
+    let dir, outPath
+    if (trailingSlash) {
+        dir = `${outDir}${basePath}${src}`
+        outPath = `${dir}/index.html`
+    } else {
+        outPath = `${outDir}${basePath}${src}`
+        if (!outPath.endsWith(".html")) {
+            outPath += ".html"
+        }
+        dir = dirname(outPath)
+    }
     const content = `<meta http-equiv=Refresh content="0; url=${basePath}${dst}" />`
     if (!fs.existsSync(dir)) {
         console.log(`mkdir: ${dir}`)
